1275e92187bbb76a1d30df04e7485576c9e41741
Mohamed <azouakanmed@gmail.com>
2023-10-01 12:05:22 +0200

    adding constructor example

---
+++ src/main/java/com/azouakan/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,11 @@
+package com.azouakan.spring6restmvc.controller;
+
+import com.azouakan.spring6restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@AllArgsConstructor
+@Controller
+public class BeerController {
+    private final BeerService beerService;
+}
--- src/main/java/com/azouakan/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/azouakan/spring6restmvc/services/BeerServiceImpl.java
@@ -2,11 +2,13 @@ package com.azouakan.spring6restmvc.services;
 
 import com.azouakan.spring6restmvc.model.Beer;
 import com.azouakan.spring6restmvc.model.BeerStyle;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
