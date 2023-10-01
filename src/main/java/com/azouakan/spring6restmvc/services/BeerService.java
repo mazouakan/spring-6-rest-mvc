16f7bd549cebf2df17c8283c844094ac5fb069c6
Mohamed <azouakanmed@gmail.com>
2023-10-01 13:03:52 +0200

    adding list example

--- src/main/java/com/azouakan/spring6restmvc/services/BeerService.java
+++ src/main/java/com/azouakan/spring6restmvc/services/BeerService.java
@@ -2,9 +2,11 @@ package com.azouakan.spring6restmvc.services;
 
 import com.azouakan.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
 
     Beer getBeerById(UUID id);
+    List<Beer> listBeers();
 }
--- src/main/java/com/azouakan/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/azouakan/spring6restmvc/services/BeerServiceImpl.java
@@ -7,11 +7,58 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
+
+    Map<UUID,Beer> beerMap;
+
+    public BeerServiceImpl() {
+
+        this.beerMap = new HashMap<>();
+
+        Beer beer1 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        Beer beer2 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Crank")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356222")
+                .price(new BigDecimal("11.99"))
+                .quantityOnHand(392)
+                .createDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        Beer beer3 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Sunshine City")
+                .beerStyle(BeerStyle.IPA)
+                .upc("12356")
+                .price(new BigDecimal("13.99"))
+                .quantityOnHand(144)
+                .createDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        beerMap.put(beer1.getId(),beer1);
+        beerMap.put(beer2.getId(),beer2);
+        beerMap.put(beer3.getId(),beer3);
+    }
 
     @Override
     public Beer getBeerById(UUID id) {
@@ -29,5 +76,10 @@ public class BeerServiceImpl implements BeerService {
                 .createDate(LocalDateTime.now())
                 .updateDate(LocalDateTime.now())
                 .build();
+    }
+
+    @Override
+    public List<Beer> listBeers() {
+        return new ArrayList<>(beerMap.values());
     }
 }
