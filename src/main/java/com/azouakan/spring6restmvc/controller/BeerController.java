48b4eb40b65da78cf6b6567553f95795faafaed0
Mohamed <azouakanmed@gmail.com>
2023-10-01 16:22:16 +0200

    adding get beer by id

--- src/main/java/com/azouakan/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/azouakan/spring6restmvc/controller/BeerController.java
@@ -4,7 +4,9 @@ import com.azouakan.spring6restmvc.model.Beer;
 import com.azouakan.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,16 +15,18 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping(value = "/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id){
+    @RequestMapping(value = "{beerId}",method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
         log.debug("Get Beer by Id - in controller");
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
