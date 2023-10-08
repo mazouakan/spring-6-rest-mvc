9c0c26fa65ef49494653510d034f95b606b791cb
Mohamed <azouakanmed@gmail.com>
2023-10-08 18:50:39 +0200

    adding tests for customer

---
+++ src/test/java/com/azouakan/spring6restmvc/controller/CustomerControllerTest.java
@@ -0,0 +1,51 @@
+package com.azouakan.spring6restmvc.controller;
+
+import com.azouakan.spring6restmvc.model.Customer;
+import com.azouakan.spring6restmvc.services.CustomerService;
+import com.azouakan.spring6restmvc.services.CustomerServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CustomerService customerService;
+
+    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+    @Test
+    void customerList() throws Exception {
+        given(customerService.customersList()).willReturn(customerServiceImpl.customersList());
+        mockMvc.perform(get("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+    }
+
+    @Test
+    void getCustomerById() throws Exception {
+        Customer testCustomer = customerServiceImpl.customersList().get(0);
+        given(customerService.getCustomerById(testCustomer.getId()))
+                .willReturn(customerServiceImpl.getCustomerById(testCustomer.getId()));
+
+        mockMvc.perform(get("/api/v1/customer/" + testCustomer.getId().toString())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testCustomer.getId().toString())))
+                .andExpect(jsonPath("$.customerName", is(testCustomer.getCustomerName())));
+    }
+}
