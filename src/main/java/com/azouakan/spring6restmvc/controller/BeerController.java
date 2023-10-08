7736167715c1fdc910826dc53fda07e881363b27
Mohamed <azouakanmed@gmail.com>
2023-10-08 17:04:30 +0200

    adding mockmvc config

--- src/main/java/com/azouakan/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/azouakan/spring6restmvc/controller/BeerController.java
@@ -2,7 +2,7 @@ package com.azouakan.spring6restmvc.controller;
 
 import com.azouakan.spring6restmvc.model.Beer;
 import com.azouakan.spring6restmvc.services.BeerService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1/beer")
 public class BeerController {
