6ea4e4e69ac6f6243843fc60be44b2f0c3f3e4c5
Mohamed <azouakanmed@gmail.com>
2023-10-15 17:23:38 +0200

    adding jpa bootstrap

---
+++ src/test/java/com/azouakan/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -0,0 +1,33 @@
+package com.azouakan.spring6restmvc.bootstrap;
+
+import com.azouakan.spring6restmvc.repositories.BeerRepository;
+import com.azouakan.spring6restmvc.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class BootstrapDataTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+
+    BootstrapData bootstrapData;
+
+    @BeforeEach
+    void setUp(){
+        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+    }
+
+    @Test
+    void testRun() throws Exception {
+        bootstrapData.run();
+        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(customerRepository.count()).isEqualTo(3);
+    }
+}
